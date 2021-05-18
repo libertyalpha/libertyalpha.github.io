54ff6cc9554fa8bbc08a7f646f735d29e791491c
libertyalpha <79082229+libertyalpha@users.noreply.github.com>
2021-05-18 20:46:44 +0530

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,7 +5,7 @@ function scrollToSection(event) {
     event.preventDefault();
     var $section = $($(this).attr('href')); 
     $('html, body').animate({
-      scrollTop: $section.offset().top
+      scrollTop: $section.offset().top  - 130
     }, 500);
   }
   $('[data-scroll]').on('click', scrollToSection);
@@ -15,22 +15,36 @@ function scrollToSection(event) {
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 170) {
-	    $(".navbar").css({"background-color": "yellow", "transition": ".7s"});
+	    $(".navbar").css({"background-color": "#d7d7df ", "transition": ".7s"});
 	  }
 
 	  else{
-		  $(".navbar").css("background" , "transparent");  	
+		  $(".navbar").css("background" , "#fff");  	
 	  }
 	  
 	  if (scroll > 480) {
-	    $(".navi-sigaret").css({"background-color": "yellow", "transition": ".7s"});
+	    $(".navi-sigaret").css({"background-color": "#d7d7df", "transition": ".7s"});
 	  }
 
 	  else{
-		  $(".navi-sigaret").css("background" , "transparent");  	
+		  $(".navi-sigaret").css("background" , "#fff");  	
 	  }
   })
+// BACK TO TOP
 
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
 
 
 
